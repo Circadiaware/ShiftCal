1946a8d4de5dc2ec871df1289b658b0b56ba8220
Nulide <Nulide@tutanota.de>
2022-05-07 20:38:06 +0200

    FixedBug: Ringtone does not play sometimes.

--- app/src/main/java/de/nulide/shiftcal/ExecutedAlarmActivity.java
+++ app/src/main/java/de/nulide/shiftcal/ExecutedAlarmActivity.java
@@ -2,9 +2,11 @@ package de.nulide.shiftcal;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.media.AudioAttributes;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.view.View;
@@ -82,6 +84,15 @@ public class ExecutedAlarmActivity extends AppCompatActivity implements View.OnC
 
         Uri uri = Uri.parse(settings.getSetting(Settings.SET_ALARM_TONE));
         ringtone = RingtoneManager.getRingtone(this, uri);
+        ringtone.setStreamType(RingtoneManager.TYPE_ALARM);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+           AudioAttributes aa = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
+                    .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+                    .build();
+            ringtone.setAudioAttributes(aa);
+        }
         ringtone.play();
     }
 
